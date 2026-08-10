71816d2ca45b7784fec21c8c3622ffb33ef78849
SolarPH <49668966+SolarPH@users.noreply.github.com>
2026-08-10 17:02:16 +0800

    Initial Project

---
+++ ModernUI_NET472/Buttons/HighlightImageButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModernUI_NET472.Buttons
+{
+    public class HighlightImageButton : Control
+    {
+        private Label ctrlText;
+        public HighlightImageButton()
+        {
+            ctrlText = new Label();
+            ctrlText.Text = string.Empty;
+        }
+
+        private void updateControlInitials()
+        {
+            if (string.IsNullOrEmpty(ctrlText.Text) && !string.IsNullOrEmpty(Name))
+            {
+                ctrlText.Text = Name;
+            }
+        }
+
+        #region Overrides
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            updateControlInitials();
+        }
+
+        protected override void OnParentChanged(EventArgs e)
+        {
+            base.OnParentChanged(e);
+            updateControlInitials();
+        }
+        #endregion
+    }
+}
